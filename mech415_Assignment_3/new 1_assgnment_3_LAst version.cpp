fd553db42b2339883be187882e8e200e9ee30cef
Irwin514 <48497496+Irwin514@users.noreply.github.com>
2021-01-19 06:09:22 -0500

    Add files via upload

---
+++ mech415_Assignment_3/new 1_assgnment_3_LAst version.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+
+class Array {	
+				int *A;				//use pointer to hold a dynamic 1d array
+				
+				public:
+				int n,m;
+				
+						void Array(int &n, int &m)
+						void
+						void
+						void
+			}
+			
+int main()
+	{	int arr[3][4]={{1,3,7,11},{5,6,9,7},{9,17,11,14}};
+		arr2
+	
+Array::~Array()			//a destructor
+{	cout<<" \n smash Destructing !";
+	
+	if(A != NULL)
+			{	delete A;
+				A = NULL;
+			}
+	else { cout<< "\n ERROR in DESTRUCTOR";}
+	
+}
+		
+		
+		
+		
+
